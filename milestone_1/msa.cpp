101cac188eeadba70f4614b9a476e0d43f4ad8d5
seb_wsl <seb_porras@DESKTOP-MOJB6KA>
2023-08-06 16:25:36 +1000

    set up lower diaganonal matrix to calc distances

--- milestone_1/msa.cpp
+++ milestone_1/msa.cpp
@@ -10,7 +10,7 @@ int main (int argc, char** argv) {
 
     if (argc == 1) {
         std::cout << "Provide a fasta file" << std::endl;
-        return 1; 
+        return CLI_ERROR; 
     }
 
     //data structure to hold our sequences 
@@ -18,14 +18,29 @@ int main (int argc, char** argv) {
     seqs->numSeqs = 0;
 
     read_fasta_file(argv[FILENAME], seqs);//populate with sequences
-    std::cout << "num seqs " << seqs->numSeqs << std::endl;
-    
-    for (const auto& pair : seqs->ids) {
-        std::cout << pair.first << " " << pair.second << std::endl; 
-        std::cout << seqs->seqs[pair.second] << std::endl; 
+                                          //
+    int matDims = seqs->numSeqs;
+    //construct a lower diagonal matrix
+    int* distances = (int*) malloc(sizeof(int) * (matDims * (matDims + 1) / 2)); 
+
+    for (int i = 0; i < matDims; ++i) {
+        for (int j = 0; j < matDims; ++j) {
+            if (i >= j) { //only inspect lower half of matrix 
+                calc_dist(distances, i, j, seqs); 
+            }
+        }
     }
 
+    free(distances);
+
     return 0;
+}
+
+
+void calc_dist(int* distances, int i, int j, std::unique_ptr<Sequences>& seqs) {
+
+    //dynamicAlign()
+    //dists[i * (i - 1) / 2 + (j - 1) = scoreAlignment(seqI, seqJ);
 }
 
 void read_fasta_file(std::string fileName, std::unique_ptr<Sequences>& seqs) {
@@ -34,13 +49,12 @@ void read_fasta_file(std::string fileName, std::unique_ptr<Sequences>& seqs) {
 
     if (!file.is_open()) {
         std::cout << "File could not be opened" << std::endl; 
-        exit(2);
+        exit(FILE_ERROR);
     }
 
     std::string line; 
     std::string currentSeq;
     std::string currentId; 
-
     while (std::getline(file, line)) {
 
         if (line[0] == '>') {
@@ -58,7 +72,6 @@ void read_fasta_file(std::string fileName, std::unique_ptr<Sequences>& seqs) {
 
     //save the last sequence 
     seqs->seqs.push_back(currentSeq);
-
     file.close();
 }
 
--- milestone_1/msa.h
+++ milestone_1/msa.h
@@ -15,13 +15,17 @@
 const int MAX_SEQ_LEN = 200;
 const int FILENAME = 1;
 
+const int CLI_ERROR = 1;
+const int FILE_ERROR = 2;
+
 struct Sequences {
     int numSeqs;
     std::vector<std::string> seqs;
-    std::unordered_map<std::string, int> ids; 
+    std::map<std::string, int> ids; 
 };
 
 
 void read_fasta_file(std::string fileName, std::unique_ptr<Sequences>& seqs); 
+void calc_dist(int* distances, int i, int j, std::unique_ptr<Sequences>& seqs);
 
 #endif
