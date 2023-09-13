2e2fffe091eabef113c52c72e0a63bf216b0e332
seb <sebastian.porras01@gmail.com>
2023-09-13 22:08:51 +1000

    adds openMP to cpu implementation

--- matrix_multiply/matrixMultiply.cpp
+++ matrix_multiply/matrixMultiply.cpp
@@ -15,45 +15,58 @@ void matrixMultiply(int N, const float* A, const float* B, float* C,
   
     memset(C, 0.0f, N * N * sizeof(float));
 
-    const int ROLL_FACTOR = 4; 
+    const int ROLL_FACTOR = 8; 
+    const int VEC_LEN = 8; 
 
-    omp_set_num_threads(32);
+    omp_set_num_threads(64);
 
-    for (int i = 0; i < N; i += 8 * ROLL_FACTOR) {   
+    for (int i = 0; i < N; i += VEC_LEN * ROLL_FACTOR) {   
 
         #pragma omp parallel for 
         for (int j = 0; j < N; j++) {
 
-            __m256 c1 = _mm256_load_ps(&C[i + j * N]);
-            __m256 c2 = _mm256_load_ps(&C[i + 8 + j * N]);
-            __m256 c3 = _mm256_load_ps(&C[i + 16 + j * N]);
-            __m256 c4 = _mm256_load_ps(&C[i + + 24 + j * N]);
+            __m256 output_vecs[ROLL_FACTOR]; 
 
-            
+            for (int x = 0; x < ROLL_FACTOR; x += 4) {
+                output_vecs[x] = _mm256_load_ps(&C[i + (VEC_LEN * x) + j * N]);
+                output_vecs[x + 1] = _mm256_load_ps(&C[i + (VEC_LEN * (x + 1)) + j * N]);
+                output_vecs[x + 2] = _mm256_load_ps(&C[i + (VEC_LEN * (x + 2)) + j * N]);
+                output_vecs[x + 3] = _mm256_load_ps(&C[i + (VEC_LEN * (x + 3)) + j * N]);
+            }
+                  
             for (int k = 0; k < N; k++) {
 
                 __m256 b = _mm256_broadcast_ss(&B[j * N + k]);
                 
-                c1 = _mm256_add_ps(c1,
-                 _mm256_mul_ps(_mm256_load_ps(&A[k * N + i]), b));
-                
-                c2 = _mm256_add_ps(c2,
-                 _mm256_mul_ps(_mm256_load_ps(&A[k * N + 8 + i]), b));
+                for (int x = 0; x < ROLL_FACTOR; x += 4) {
+                    output_vecs[x] = _mm256_add_ps(output_vecs[x],
+                                     _mm256_mul_ps(
+                                     _mm256_load_ps(
+                                        &A[k * N + (VEC_LEN * x) + i]), b));
 
-                c3 = _mm256_add_ps(c3,
-                 _mm256_mul_ps(_mm256_load_ps(&A[k * N + 16 +  i]), b));
+                    output_vecs[x + 1] = _mm256_add_ps(output_vecs[x + 1],
+                                     _mm256_mul_ps(
+                                     _mm256_load_ps(
+                                        &A[k * N + (VEC_LEN * (x + 1)) + i]), b));
 
-                c4 = _mm256_add_ps(c4,
-                 _mm256_mul_ps(_mm256_load_ps(&A[k * N + 24 + i]), b));
+                    output_vecs[x + 2] = _mm256_add_ps(output_vecs[x + 2],
+                                     _mm256_mul_ps(
+                                     _mm256_load_ps(
+                                        &A[k * N + (VEC_LEN * (x + 2)) + i]), b));
+
+                    output_vecs[x + 3] = _mm256_add_ps(output_vecs[x + 3],
+                                     _mm256_mul_ps(
+                                     _mm256_load_ps(
+                                        &A[k * N + (VEC_LEN * (x + 3)) + i]), b));
+                }   
             }    
 
-            _mm256_store_ps(&C[i + j * N], c1);
-
-            _mm256_store_ps(&C[i + 8 + j * N], c2);
-
-            _mm256_store_ps(&C[i + 16 + j * N], c3);
-
-            _mm256_store_ps(&C[i + 24 + j * N], c4);
+            for (int x = 0; x < ROLL_FACTOR; x += 4) {
+                _mm256_store_ps(&C[i + (x * VEC_LEN) + j * N], output_vecs[x]);
+                _mm256_store_ps(&C[i + ((x + 1) * VEC_LEN) + j * N], output_vecs[x + 1]);
+                _mm256_store_ps(&C[i + ((x + 2) * VEC_LEN) + j * N], output_vecs[x + 2]);
+                _mm256_store_ps(&C[i + ((x + 3) * VEC_LEN) + j * N], output_vecs[x + 3]);
+            }
         }
     }
 }
