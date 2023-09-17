0cf4a3eaa2189ab36ee7c867a2c457a09a3d9bdd
seb <sebastian.porras01@gmail.com>
2023-09-17 19:56:47 +1000

    working mpi

--- matrix_multiply/matrixMultiplyMPI.cpp
+++ matrix_multiply/matrixMultiplyMPI.cpp
@@ -3,29 +3,75 @@
 void matrixMultiply_MPI(int N, const float* A, const float* B, 
                         float* C, int* args, int argCount) {
 
-    int worldSize; 
-    MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
 
-    int rank;
+    int worldSize, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    int NPerRank = int(float(N * N) / float(worldSize)); //(2048 * 2048) / 2
+    MPI_Status status;
+    //define some unique pieces based on rank and world size 
+    int NPerRank = int(float(N) / float(worldSize)); 
+    int myFirstN = rank * NPerRank;
+    int myLastN = (rank + 1) * NPerRank; 
+    
+    memset(C, 0.0f, N * N * sizeof(float));
+    
+    const int ROLL_FACTOR = 8; 
+    const int VEC_LEN = 8; 
 
-    int firstN = (rank  == 0) ? 0 : NPerRank; 
-    int lastN = (rank == 0) ? NPerRank : N * N; //2048 * 2048
+    omp_set_num_threads(256);
 
-    float* localC = (float*) malloc(sizeof(float) * (N * N)); 
-    float* localA = (float*) malloc(sizeof(float) * NPerRank);
-    for (int i = 0; i < NPerRank; ++i) {
-        localA[i] = A[i + firstN]; 
+    for (int i = 0; i < N; i += VEC_LEN * ROLL_FACTOR) {  
+        
+        #pragma omp parallel for 
+        for (int j = myFirstN; j < myLastN; j++) {
+
+            __m256 output_vecs[ROLL_FACTOR]; 
+                        
+            for (int x = 0; x < ROLL_FACTOR; x += 4) {
+                output_vecs[x] = _mm256_load_ps(&C[i + (VEC_LEN * x) + j * N]);
+                output_vecs[x + 1] = _mm256_load_ps(&C[i + (VEC_LEN * (x + 1)) + j * N]);
+                output_vecs[x + 2] = _mm256_load_ps(&C[i + (VEC_LEN * (x + 2)) + j * N]);
+                output_vecs[x + 3] = _mm256_load_ps(&C[i + (VEC_LEN * (x + 3)) + j * N]);
+            }
+                
+            for (int k = 0; k < N; k++) {
+
+                __m256 b = _mm256_broadcast_ss(&B[j * N + k]);
+                
+                for (int x = 0; x < ROLL_FACTOR; x += 4) {
+                    output_vecs[x] = _mm256_add_ps(output_vecs[x],
+                                     _mm256_mul_ps(
+                                     _mm256_load_ps(
+                                        &A[k * N + (VEC_LEN * x) + i]), b));
+
+                    output_vecs[x + 1] = _mm256_add_ps(output_vecs[x + 1],
+                                     _mm256_mul_ps(
+                                     _mm256_load_ps(
+                                        &A[k * N + (VEC_LEN * (x + 1)) + i]), b));
+
+                    output_vecs[x + 2] = _mm256_add_ps(output_vecs[x + 2],
+                                     _mm256_mul_ps(
+                                     _mm256_load_ps(
+                                        &A[k * N + (VEC_LEN * (x + 2)) + i]), b));
+
+                    output_vecs[x + 3] = _mm256_add_ps(output_vecs[x + 3],
+                                     _mm256_mul_ps(
+                                     _mm256_load_ps(
+                                        &A[k * N + (VEC_LEN * (x + 3)) + i]), b));
+                }   
+            }    
+
+            for (int x = 0; x < ROLL_FACTOR; x += 4) {
+                _mm256_store_ps(&C[i + (x * VEC_LEN) + j * N], output_vecs[x]);
+                _mm256_store_ps(&C[i + ((x + 1) * VEC_LEN) + j * N], output_vecs[x + 1]);
+                _mm256_store_ps(&C[i + ((x + 2) * VEC_LEN) + j * N], output_vecs[x + 2]);
+                _mm256_store_ps(&C[i + ((x + 3) * VEC_LEN) + j * N], output_vecs[x + 3]);
+            }
+        }
     }
 
-    matrixMultiply(NPerRank, localA, B, localC, args, argCount); 
-    MPI_Allgather(localC, NPerRank, MPI_FLOAT, C, NPerRank,
-     MPI_FLOAT, MPI_COMM_WORLD); 
-
-
-    free(localA); 
-    free(localC); 
+    MPI_Gather(C + myFirstN * N, NPerRank * N, MPI_FLOAT, C, NPerRank * N, MPI_FLOAT, 
+                0, MPI_COMM_WORLD);
+    MPI_Bcast(C, N * N, MPI_FLOAT, 0, MPI_COMM_WORLD);   
 }
-
